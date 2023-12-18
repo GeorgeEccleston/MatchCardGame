7cdc753cbf4bc19f7e2e428acb433508561084af
george.eccleston@me.com <george.eccleston@me.com>
2023-12-18 13:40:05 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by GeorgeAdmin on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
