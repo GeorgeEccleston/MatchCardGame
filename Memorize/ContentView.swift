aa242b72b0604b383bdcf26a633f5a07cb934397
george.eccleston@me.com <george.eccleston@me.com>
2023-12-18 19:28:43 -0700

    Lession 1 CS193P 2023

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["👽","🍎","🍋","👁️"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            ForEach(0...3) { index in
+                CardView(emoji: emojis[index], isFaceUp: true)
+            }
+                
+//            CardView(emoji: emojis[1], isFaceUp: false)
+//            CardView(emoji: emojis[2], isFaceUp: true)
+//            CardView(emoji: emojis[3], isFaceUp: true)
         }
+        .foregroundColor(/*@START_MENU_TOKEN@*/.orange/*@END_MENU_TOKEN@*/)
         .padding()
     }
 }
+
+struct CardView: View {
+    let emoji: String
+    var isFaceUp = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 10)
+                    .strokeBorder(lineWidth: 3)
+                Text(emoji)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+            } else {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(.orange)
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
